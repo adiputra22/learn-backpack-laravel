c9c23bcea3e6766936771bdba9d4cb13dc00e0cb
adiputra22 <tuts.adiputra@gmail.com>
2021-05-28 07:56:42 +0700

    initial backpack

---
+++ app/Models/Article.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Article extends Model
+{
+    use \Backpack\CRUD\app\Models\Traits\CrudTrait;
+    use HasFactory, SoftDeletes;
+
+    const STATUS_DRAFT = 'DRAFT';
+    const STATUS_PUBLISHED = 'PUBLISHED';
+
+    const IS_FEATURED = 1;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'category_id',
+        'title',
+        'slug',
+        'content',
+        'image',
+        'status',
+        'date',
+        'featured',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'id' => 'integer',
+        'category_id' => 'integer',
+        'date' => 'date',
+        'featured' => 'boolean',
+    ];
+
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function tags()
+    {
+        return $this->belongsToMany(\App\Models\Tag::class);
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function category()
+    {
+        return $this->belongsTo(\App\Models\Category::class);
+    }
+}
